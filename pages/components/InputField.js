2a35781483fc6e185623dbd4d0f6ed4caf7e6d45
Hemant <hemantkumarsonkar1@gmail.com>
2024-05-01 21:05:51 +0530

    React forward ref 1 completed

--- pages/components/InputField.js
+++ pages/components/InputField.js
@@ -2,9 +2,12 @@
 import React from 'react';
 
 const InputField = React.forwardRef((props, ref) => {
+  const { values, setValues } = props;
   return (
     <div>
-      <input id='input' type='text' ref={ref} />
+      <input id='input' type='text' ref={ref} value={values} onChange={(e) => {
+        setValues(e.target.value);
+      }}/>
     </div>
   );
 });
--- pages/index.js
+++ pages/index.js
@@ -2,23 +2,24 @@ import React, { useRef, useState } from 'react';
 import InputField from '../pages/components/InputField';
 
 function Home() {
-  const newRef = useRef();
+  const newRef = useRef("");
   const [values, setValues] = useState('');
 
   const focusInput = () => {
-
+    newRef.current.focus();
   };
   const settingValue = () => {
-
+    document.getElementById('textarea').value = values;
+    
   };
 
   return (
     <div>
-    <InputField  ref={newRef}  type="text"/>
+    <InputField  ref={newRef}  type="text" setValues={setValues}/>
     <br/>
     <button id="settingValueButton" onClick={settingValue}>Set Value</button>
     <button id="focusInputButton" onClick={focusInput}>Focus the input</button><br/><br/>
-     <textarea id="textarea" value={values}></textarea>
+     <textarea id="textarea"></textarea>
 
     </div>
   );
